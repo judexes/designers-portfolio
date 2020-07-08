133d4578e560b15a4f7b2190ae5f4dd9f4e972c6
jude okoroafor <judeokoroafor@gmail.com>
2020-07-09 00:24:42 +0100

    Update and documentation

--- index.php
+++ index.php
@@ -12,114 +12,76 @@
         <meta name="robots" content="index, follow">
         <meta name="language" content="English">
         <meta name="keywords" content="Template, HTML, YourDesigner, Designer">
-
         <!-- Hero CSS -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.css">
         <!-- Bootstrap core CSS -->
         <link href="style/vendors/bootstrap.min.css" rel="stylesheet">
         <!-- Custom styles for this template -->
         <link href="style/main.css" rel="stylesheet">
     </head>
     <body class="bg-light">
-        <section>
+        <section class="">
             <!-- ------------------------ MAIN NAV ----------------------- -->
-            <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
-                <a class="navbar-brand mr-auto mr-lg-0" href="#">Offcanvas navbar</a>
+            <nav class="navbar navbar-expand-lg fixed-top navbar-dark navbar-main ">
+                <a class="navbar-brand mr-auto mr-lg-0" href="#">
+                    <img src="img/logo.png" alt="Logo Image">
+                </a>
+                <div class="navbar-collapse offcanvas-collapse" id="navbarsExampleDefault">
+                    <ul class="navbar-nav mr-auto text-uppercase mx-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Services</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Portfolio</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Testimonial</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Resume</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Blog</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Hire me</a>
+                        </li>
+                        <span class="d-block d-sm-none" id="">
+                            <img src="img/twitter.png" alt="image">
+                            <img src="img/twitter.png" alt="image">
+                            <img src="img/twitter.png" alt="image">
+                            <img src="img/twitter.png" alt="image">
+                        </span>
+                    </ul>
+                </div>
+                <a class="navbar-brand mr-auto mr-lg-0 d-none d-sm-block" href="#">
+                    
+                    <img src="img/twitter.png" alt="image">
+                    <img src="img/twitter.png" alt="image">
+                    <img src="img/twitter.png" alt="image">
+                    <img src="img/twitter.png" alt="image">
+                </a>
                 <button class="navbar-toggler p-0 border-0" type="button" data-toggle="offcanvas">
                 <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="navbar-collapse offcanvas-collapse" id="navbarsExampleDefault">
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="#">Dashboard <span class="sr-only">(current)</span></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Notifications</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Profile</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Switch account</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Settings</a>
-                            <div class="dropdown-menu" aria-labelledby="dropdown01">
-                                <a class="dropdown-item" href="#">Action</a>
-                                <a class="dropdown-item" href="#">Another action</a>
-                                <a class="dropdown-item" href="#">Something else here</a>
-                            </div>
-                        </li>
-                    </ul>
-                    <form class="form-inline my-2 my-lg-0">
-                        <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                    </form>
-                </div>
             </nav>
             <!----------------------- SECONDARY NAV ------------------------->
-            <div class="nav-scroller bg-white shadow-sm">
-                <nav class="nav nav-underline">
-                    <a class="nav-link active" href="#">Dashboard</a>
-                    <a class="nav-link" href="#">
-                        Friends
-                        <span class="badge badge-pill bg-light align-text-bottom">27</span>
-                    </a>
-                    <a class="nav-link" href="#">Explore</a>
-                    <a class="nav-link" href="#">Suggestions</a>
-                    <a class="nav-link" href="#">Link</a>
-                    <a class="nav-link" href="#">Link</a>
-                    <a class="nav-link" href="#">Link</a>
-                    <a class="nav-link" href="#">Link</a>
-                    <a class="nav-link" href="#">Link</a>
-                </nav>
+        </section>
+        <section class="hero-section">
+            <div class="splash">
+                <div class="container">
+                    <h1 class="display-3">Hello, I’m Folio</h1>
+                    <p class="">Let’s Create Something new and awesome Togeather. I can help you create positive and permanent changes in your business.
+                    </p>
+                    <p><a class="btn btn-primary btn-lg text-uppercase" href="#" role="button">download resume</a></p>
+                </div>
             </div>
         </section>
-        <section class="hero">
-            <section>
-                <div id="slider-animation" class="carousel slide" data-ride="carousel">
-                    <!-- Indicators -->
-                    <ul class="carousel-indicators">
-                        <li data-target="#slider-animation" data-slide-to="0" class="active"></li>
-                        <li data-target="#slider-animation" data-slide-to="1"></li>
-                        <li data-target="#slider-animation" data-slide-to="2"></li>
-                    </ul>
-                    <!-- The slideshow -->
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                            <img src="https://i.pinimg.com/originals/10/1b/bb/101bbb8a4e78d2a5d9589d60e410dac8.jpg" alt="Los Angeles">
-                            <div class="text-box">
-                                <h2 class="wow slideInRight" data-wow-duration="2s">This is Obitope text</h2>
-                                <p class="wow slideInLeft" data-wow-duration="2s">There is now an abundance of readable dummy texts. These are usually used when a text is required purely to fill a space. </p>
-                            </div>
-                        </div>
-                        <div class="carousel-item">
-                            <img src="http://en.ddpai.com/webcommon/images/mix3/animation/bac-2.png" alt="Chicago">
-                            <div class="text-box">
-                                <h2 class="wow slideInUp" data-wow-duration="4s" >This is samuel text</h2>
-                                <p class="wow fadeInDown" data-wow-duration="4s">There is now an abundance of readable dummy texts. These are usually used when a text is required purely to fill a space. </p>
-                            </div>
-                        </div>
-                        <div class="carousel-item">
-                            <img src="https://playthunee.com/assets/images/4player-bg.jpg" alt="New York">
-                            <div class="text-box">
-                                <h2 class="wow fadeInUp" data-wow-duration="4s">This is Airborne text</h2>
-                                <p class="wow fadeInUp" data-wow-duration="2s">There is now an abundance of readable dummy texts. These are usually used when a text is required purely to fill a space. </p>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- Left and right controls -->
-                    <a class="carousel-control-prev" href="#slider-animation" data-slide="prev">
-                        <span class="carousel-control-prev-icon"></span>
-                    </a>
-                    <a class="carousel-control-next" href="#slider-animation" data-slide="next">
-                        <span class="carousel-control-next-icon"></span>
-                    </a>
-                </div>
-                
-            </section>
-        </section>
-
         
         <script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js"></script>
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
