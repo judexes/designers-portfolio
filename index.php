122067e94d87fef7a82115758a6a2d619666ccb6
jude okoroafor <judeokoroafor@gmail.com>
2020-07-09 23:00:46 +0100

    Update to index.php and main.css
    change image name also

--- index.php
+++ index.php
@@ -17,8 +17,11 @@
         <link href="style/vendors/bootstrap.min.css" rel="stylesheet">
         <!-- Custom styles for this template -->
         <link href="style/main.css" rel="stylesheet">
+        
     </head>
     <body class="bg-light">
+        <div id="top"></div>
+        <!-- MENU SECTION -->
         <section class="">
             <!-- ------------------------ MAIN NAV ----------------------- -->
             <nav class="navbar navbar-expand-lg fixed-top navbar-dark navbar-main ">
@@ -28,34 +31,35 @@
                 <div class="navbar-collapse offcanvas-collapse" id="navbarsExampleDefault">
                     <ul class="navbar-nav mr-auto text-uppercase mx-auto">
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Home</a>
+                            <a class="nav-link" href="#top">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">About</a>
+                            <a class="nav-link" href="#about">About</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Services</a>
+                            <a class="nav-link" href="#services">Services</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Portfolio</a>
+                            <a class="nav-link" href="#portfolio">Portfolio</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Testimonial</a>
+                            <a class="nav-link" href="#testimonial">Testimonial</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Resume</a>
+                            <a class="nav-link" href="#resume">Resume</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Blog</a>
+                            <a class="nav-link" href="#blog">Blog</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Hire me</a>
-                        </li>
+                            <a class="nav-link" href="#hire">Hire me</a>
+                            <!--                             <a class="nav-link btn btn-primary btn-lg" href="#" role="button">Hire me</a>
+                        -->                        </li>
                         <span class="align-bottom d-block d-sm-none fixed-bottom mb-5 socials text-center" id="">
-                    <img src="img/twitter.png" alt="image">
-                    <img src="img/facebook.png" alt="image">
-                    <img src="img/behance.png" alt="image">
-                    <img src="img/instagram.png" alt="image">
+                            <img src="img/twitter.png" alt="image">
+                            <img src="img/facebook.png" alt="image">
+                            <img src="img/behance.png" alt="image">
+                            <img src="img/instagram.png" alt="image">
                         </span>
                     </ul>
                 </div>
@@ -71,6 +75,7 @@
             </nav>
             <!----------------------- SECONDARY NAV ------------------------->
         </section>
+        <!-- HERO SECTION -->
         <section class="hero-section">
             <div class="splash">
                 <div class="container">
@@ -79,11 +84,153 @@
                     </p>
                     <p><a class="btn btn-primary btn-lg text-uppercase" href="#" role="button">download resume</a></p>
                 </div>
+                <div id="about"></div>
             </div>
         </section>
-        
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js"></script>
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-        <script src="javascript/vendors/bootstrap.bundle.js"></script>
-        <script src="javascript/script.js"></script></body>
-    </html>
+        <!-- ABOUT SECTION -->
+        <section class="about-us-section">
+            
+            <div class="container">
+                
+                <div class="row text-center ">
+                    <div class="col-sm-6">
+                        <div class="about-us about-us-description ml-2 mr-2 text-left">
+                            <h2 class="title font-weight-bold">I am a professional<br>designer from New York</h2>
+                            <p class="title-description">I can help you create positive and permanent changes in your life. High Performance Portfolio Template. Let’s Create Something new and awesome Togeather. I can help you create positive and permanent changes in your life..</p>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="about-us mb-3 mr-sm-0 ml-sm-0 profile">
+                            
+                            <img class="img-fluid" src="img/about.png">
+                        </div>
+                    </div>
+                </div>
+                <div id="services"></div>
+            </div>
+        </section>
+        <!-- SERVICES SECTION -->
+        <section class="services-section">
+            
+            <div class="container text-center">
+                
+                <div class="row services">
+                    <div class="px-3 py-3 pb-md-4 mx-auto text-center col-md-8 offset-md-2 ">
+                        <h6 class="subtitle font-weight-bold">Services</h6>
+                        <h2 class="title font-weight-bold">From Concept To Creation</h5>
+                        <p class="title-description ml-4 mr-4">Let’s Create Something new and awesome Togeather.<br>I can help you create positive and permanent changes in your life..</p>
+                    </div>
+                </div>
+            </div>
+            <div class="container-fluid">
+                <div class="row text-center justify-content-center font-weight-bold">
+                    <div class="col-sm-3">
+                        <div class=" m-3">
+                            <img class="img-fluid" src="img/service1.png">
+                            <p class="mt-2">Web Ui Design</p>
+                        </div>
+                    </div>
+                    <div class="col-sm-3">
+                        <div class="m-3">
+                            <img class="img-fluid" src="img/service2.png">
+                            <p class="mt-2">Mobile Ui Design</p>
+                        </div>
+                    </div>
+                    <div class="col-sm-3">
+                        <div class="m-3">
+                            <img class="img-fluid" src="img/service3.png">
+                            <p class="mt-2">Branding Design</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="portfolio"></div>
+        </section>
+        <!-- PORTFOLIO SECTION -->
+        <section class="portfolio-section">
+            <div class="container">
+                <div class="row text-left justify-content-center font-weight-bold">
+                    <div class="col-sm-5">
+                        <div class="m-3">
+                            <img class="img-fluid" src="img/portfolio1.png">
+                            <p class="mt-2">Branding Design</p>
+                        </div>
+                    </div>
+                    <div class="col-sm-5">
+                        <div class="m-3">
+                            <div class="">
+                                <h6 class="subtitle font-weight-bold">Portfolio</h6>
+                                <h2 class="title font-weight-bold">Few selected works</h5>
+                                <p class="title-description font-weight-normal">Let’s Create Something new and awesome Togeather.</p>
+                            </div>
+                            <img class="img-fluid" src="img/portfolio2.png">
+                            <p class="mt-2 ">Mobile Ui Design</p>
+                        </div>
+                    </div>
+                    <div class="col-sm-5">
+                        <div class="m-3">
+                            <img class="img-fluid" src="img/portfolio3.png">
+                            <p class="mt-2">Branding Design</p>
+                        </div>
+                    </div>
+                    <div class="col-sm-5">
+                        <div class="m-3">
+                            <div class="d-none d-sm-block">
+                                <p class="title-description font-weight-normal">&nbsp;</p>
+                                <p class="title-description font-weight-normal">&nbsp;</p>
+                                <p class="title-description font-weight-normal">&nbsp;</p>
+                            </div>
+                            <img class="img-fluid" src="img/portfolio4.png">
+                            <p class="mt-2">Mobile Ui Design</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row justify-content-center">
+                    <div class="col-4">
+                        <button type="button" class="btn btn-primary btn-lg text-uppercase">explore more</button>
+                    </div>
+                </div>
+            </div>
+            <div id="products"></div>
+        </section>
+        <!-- EXPERIENCE SECTION -->
+        <section class="experience-section">
+            <div class="experience-shade">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-3">col</div>
+                        <div class="col-sm-3">col</div>
+                        <div class="col-sm-3">col</div>
+                        <div class="col-sm-3">col</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-4">
+                            <p>Happy Clients</p>
+                            <p class="counter">120</p>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <p>Happy Clients</p>
+                        <p class="counter">120</p>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <p>Happy Clients</p>
+                    <p class="counter">120</p>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <p>Happy Clients</p>
+                <p class="counter">120</p>
+            </div>
+        </div>
+    </div>
+</div>
+</section>
+</body>
+
+<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js"></script> -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="javascript/vendors/bootstrap.bundle.js"></script>
+<script src="javascript/script.js"></script></body>
+</html>
