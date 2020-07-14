3c8d7372f4bd25169e3761d02639860183ac41e6
jude okoroafor <judeokoroafor@gmail.com>
2020-07-14 12:02:57 +0100

    Update dbconn.php

--- includes/dbconn.php
+++ includes/dbconn.php
@@ -1,11 +1,12 @@
 
 <?php
-if(!empty($_POST["send"])) {
-	$email = $_POST["userEmail"];
+// UNCOMMENT ONLY AFTER SETTING UP DATABASE
 
-	$conn = mysqli_connect("localhost", "root", "", "scan-tc_db") or die("Connection Error: " . mysqli_error($conn));
-	mysqli_query($conn, "INSERT INTO subscribers (email) VALUES ('" . $email. "')");
-	$insert_id = mysqli_insert_id($conn);
-}
-// require_once "contact-view.php";
+// if(!empty($_POST["send"])) {
+// 	$email = $_POST["userEmail"];
+
+// 	$conn = mysqli_connect("localhost", "root", "", "scan-tc_db") or die("Connection Error: " . mysqli_error($conn));
+// 	mysqli_query($conn, "INSERT INTO subscribers (email) VALUES ('" . $email. "')");
+// 	$insert_id = mysqli_insert_id($conn);
+// }
 ?>
